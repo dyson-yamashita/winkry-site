47e110f1d1a572be704316ec3dc3c4821aeb9aa9
D. Yamashita <dyson.yamashita@gmail.com>
2019-01-27 01:17:11 +0900

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = (props) => (
+    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+        <div className="logo">
+            <span className="icon fa-heartbeat"></span>
+        </div>
+        <div className="content">
+            <div className="inner">
+                <h1>Winkry</h1>
+                <p>Image and create our future.</p>
+                <p>未来をイメージし作り出す。</p>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+Header.propTypes = {
+    onOpenArticle: PropTypes.func,
+    timeout: PropTypes.bool
+}
+
+export default Header
